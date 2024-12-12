88b43536bda2ab5c0ec619f875b1133fabb337da
unknown <herrera.fernando1209@gmail.com>
2024-12-12 00:17:25 -0300

    start seccion 2

---
+++ src/stores/bears/bears.store.ts
@@ -0,0 +1,38 @@
+import { create } from "zustand";
+
+interface Bear {
+  id: number;
+  name: string;
+}
+
+interface BearState {
+  blackBears: number;
+  polarBears: number;
+  pandaBears: number;
+
+  bears: Bear[];
+
+  increaseBlackBears: (by: number) => void;
+  increasePolarBears: (by: number) => void;
+  increasePandaBears: (by: number) => void;
+
+  doNothing: () => void;
+}
+
+export const useBearStore = create<BearState>()((set) => ({
+  blackBears: 10,
+  polarBears: 5,
+  pandaBears: 1,
+  bears: [{ id: 1, name: "Bear #1" }],
+
+  increaseBlackBears: (by: number) =>
+    set((state) => ({ blackBears: state.blackBears + by })),
+
+  increasePolarBears: (by: number) =>
+    set((state) => ({ polarBears: state.polarBears + by })),
+
+  increasePandaBears: (by: number) =>
+    set((state) => ({ pandaBears: state.pandaBears + by })),
+
+  doNothing: () => set((state) => ({ bears: [...state.bears] })),
+}));
