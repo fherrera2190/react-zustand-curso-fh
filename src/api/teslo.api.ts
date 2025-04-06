1315f26e6af7adb7a348344dc19b24c2083e3ea1
Fernando Herrera <herrera.fernando1209@gmail.com>
2025-04-06 00:53:54 -0300

    fix env var

--- src/api/teslo.api.ts
+++ src/api/teslo.api.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 import { useAuthStore } from "../stores";
-
 const tesloApi = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: import.meta.env.VITE_HOST_API,
 });
-
 
 tesloApi.interceptors.request.use((config) => {
   const token = useAuthStore.getState().token;
